947227d73dad75a679f0898105798326a545e2b9
heguilen <juan_heguilen@hotmail.com>
2023-06-08 17:25:54 -0300

    tryAddErr

--- coder-mongo/src/routers/cartsDB.router.js
+++ coder-mongo/src/routers/cartsDB.router.js
@@ -5,61 +5,104 @@ const cartRouterAtlas = Router();
 
 //CREO CARRITO
 cartRouterAtlas.post('/', async (req, res) => {
-   const newCart = await cartService.createCart();
-   res.send('Carrito creado')
+   try {
+      const newCart = await cartService.createCart();
+      res.status(204).send('Carrito creado')
 
+   } catch (err) {
+
+      res.status(404).send({ err })
+
+   }
 })
 
 //LLAMO  ATODOS LOS CARRITOS
 cartRouterAtlas.get('/', async (req, res) => {
-   const carts = await cartService.getAllCarts();
-   res.send(carts);
+   try {
+      const carts = await cartService.getAllCarts();
+      res.status(204).send(carts);
+
+   }
+   catch (err) {
+      res.status(500).send({ err });
+   }
 
 })
 
 //DEVUELVO POR ID
 cartRouterAtlas.get('/:cid', async (req, res) => {
-   const id = req.params.cid;
-   const findCart = await cartService.cartById(id);
-   res.send(findCart);
+   try {
+      const id = req.params.cid;
+
+      const findCart = await cartService.cartById(id);
+      res.status(200).send(findCart);
+
+   }
+   catch (err) {
+      res.status(501).send({ err });
+
+   }
 
 })
 
 //AGREGO PROD AL CARRITO
-cartRouterAtlas.post('/:idCart/product/:idProd/:quantity', async (req, res)=> {
-   const idCart = req.params.idCart;
-   const idProd = req.params.idProd;
-   const quantity = req.params.quantity;
-   const addProduct = await cartService.addProdCart(idCart, idProd, quantity);
-   res.send(addProduct);
-
+cartRouterAtlas.post('/:idCart/product/:idProd/:quantity', async (req, res) => {
+   try {
+      const idCart = req.params.idCart;
+      const idProd = req.params.idProd;
+      const quantity = req.params.quantity;
+      const addProduct = await cartService.addProdCart(idCart, idProd, quantity);
+      res.status(200).send(addProduct);
+   }
+   catch (err) {
+      res.status(400).send({ err })
+   }
 })
 //MODIFICO CANTIDAD DE PROD
 cartRouterAtlas.put('/:cid/product/:pid', async (req, res) => {
-   let idCart = req.params.cid;
-   let idProd = req.params.pid;
-   let quantity = req.body.quantity
+   try {
+      let idCart = req.params.cid;
+      let idProd = req.params.pid;
+      let quantity = req.body.quantity
 
-   await cartService.updateProd(idCart, idProd, quantity);
-   res.send('cantidad modificada')
+      await cartService.updateProd(idCart, idProd, quantity);
+      res.status(200).send('cantidad modificada')
+
+   } catch (err) {
+      res.status(500).send({ err });
+   }
+
 });
 
 //ELIMINO PORD DEL CARRITO
 cartRouterAtlas.delete('/:cid/products/:pid', async (req, res) => {
-   const id = req.params.cid
-   const prodId = req.params.pid
+   try {
+      const id = req.params.cid
+      const prodId = req.params.pid
 
-   let traigo = await cartService.deletProd(id, prodId);
+      let traigo = await cartService.deletProd(id, prodId);
 
-   res.send(traigo);
+      res.status(204).send(traigo);
+
+   } catch (err) {
+
+      res.status(404).send({ err })
+
+   }
 
 });
 
 //ELIMINO CARRITO
 cartRouterAtlas.delete('/:id', async (req, res) => {
-   const id = req.params.id
-   const delet = await cartService.deletCarrito(id);
-   res.send("chau carrito");
+
+   try {
+      const id = req.params.id
+      const delet = await cartService.deletCarrito(id);
+      res.status(204).send("chau carrito");
+
+   } catch (err) {
+      res.status(501).send({ err });
+   }
 
 });
 
--- coder-mongo/src/routers/productsDB.router.js
+++ coder-mongo/src/routers/productsDB.router.js
@@ -3,28 +3,32 @@ import productService from "../dao/service/products.service.js";
 
 const productsRouterAtlas = Router();
 
-//llamo prod
+//LLAMO PRODUCTOS
 productsRouterAtlas.get('/', async (req, res) => {
-    
-    //parametros que puedo recibir en el get
-    //estos dos por url
-    let limit = parseInt(req.query.limit);
-    let page = parseInt(req.query.page);
-    //estos dos por body
-    let category = req.body.category;
-    let price = parseInt(req.body.price);
+    try {
+        //parametros que puedo recibir en el get
+        //estos dos por url
+        let limit = parseInt(req.query.limit);
+        let page = parseInt(req.query.page);
+        //estos dos por body
+        let category = req.body.category;
+        let price = parseInt(req.body.price);
 
 
-    !limit ? limit = 10 : limit;
-    !page ? page = 1 : page;
-    !category ? category = false : category;
-    !price ? price = false : price;
+        !limit ? limit = 10 : limit;
+        !page ? page = 1 : page;
+        !category ? category = false : category;
+        !price ? price = false : price;
 
-    console.log(limit, page, category, price)
-    let busqueda = await productService.getSomeProducts(limit, page, category, price)
+        console.log(limit, page, category, price)
+        let busqueda = await productService.getSomeProducts(limit, page, category, price)
 
-    res.send(busqueda);
+        res.status(200).send(busqueda);
 
+    } catch (err) {
+        res.status(400).send({ err })
+
+    }
 })
 
 //agrego pord
@@ -35,24 +39,36 @@ productsRouterAtlas.post('/', async (req, res) => {
         res.send(`producto cargador ${downloadProduct}`)
     }
     catch (err) {
-        res.send(err)
+        res.status(500).send(err)
 
     }
 })
 
 //elimino por id
 productsRouterAtlas.delete('/:id', async (req, res) => {
-    const id = req.params.id;
-    const delet = await productService.deletProd(id);
-    res.send("eliminado")
+    try {
+        const id = req.params.id;
+        const delet = await productService.deletProd(id);
+        res.send("eliminado")
+
+    } catch (err) {
+
+        res.status(401).send({ err })
+
+    }
+
 })
 
 //modifico por id
 productsRouterAtlas.put('/', async (req, res) => {
-    const update = req.body
-    const updateProd = await productService.updateProd(update.id, update.key, update.valor);
+    try {
+        const update = req.body
+        const updateProd = await productService.updateProd(update.id, update.key, update.valor);
+        res.status(200).send(updateProd);
+    } catch (err) {
+        res.status(501).send({ err })
+    }
 
-    res.send(updateProd);
 })
 
 export { productsRouterAtlas };
